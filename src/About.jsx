eaf68669e6d72bc5b8e054f5ceb0b8fe4ac127fc
Ethaniel <37252200+ethaniel0@users.noreply.github.com>
2022-02-20 07:16:25 +0000

    adding alts

--- src/About.jsx
+++ src/About.jsx
@@ -14,10 +14,10 @@ import AboutCard from './components/AboutCard';
     <main className='text-center'>
       <span className='text-3xl font-bold mb-8'>About The Team</span>
       <div className='px-8 mt-8 md:grid grid-cols-4'>
-       <AboutCard name="Ethan Horowitz" img="/src/images/ethan.jpg" link='https://github.com/ethaniel0' desc="Hi! I'm a freshman at Duke University studying computer science and engineering, and I love software and website development. This is my 21st hackathon!" />
-        <AboutCard name="Chinmay Bhelke" img="/src/images/Chinmay.JPG" link='' desc="Hello World! I am currently a Full Stack Developer and DevOps Engineer and I love Software Development. :) This is my first Hackathon and it has been an amazing experience , excited to keep learning!" />
-        <AboutCard name="Luiz Astorga" img="/src/images/luizSq.jpg" link='https://www.linkedin.com/in/luiz-astorga-4720791a5/' desc="Hey Ya'll! I'm a CS major at UT-Dallas. I'm an incoming intern at USAA, and teach kids how to code on the side. I'm aspiring to become a data science consultant, let's work on a project together!" />
-        <AboutCard name="Ariel Taylor" img="/src/images/ariel2.jpg" link='' desc="Hey there! I attend an institution called STEM where I was able to represent my country at two international robotics competitions. This is my first hackathon." />
+       <AboutCard name="Ethan Horowitz" img="/src/images/ethan.jpg" alt='Ethan hanging from a tree like a monkey' link='https://github.com/ethaniel0' desc="Hi! I'm a freshman at Duke University studying computer science and engineering, and I love software and website development. This is my 21st hackathon!" />
+        <AboutCard name="Chinmay Bhelke" alt="just chilling" img="/src/images/Chinmay.JPG" link='https://www.linkedin.com/in/chinmay-bhelke-2717a6178/' desc="Hello World! I am currently a Full Stack Developer and DevOps Engineer and I love Software Development. :) This is my first Hackathon and it has been an amazing experience , excited to keep learning!" />
+        <AboutCard name="Luiz Astorga" img="/src/images/luizSq.jpg" link='https://www.linkedin.com/in/luiz-astorga-4720791a5/' alt='Luiz feeding a super sugary pink-popsicle to a wild squirrel while he smiles with excitement' desc="Hey Ya'll! I'm a CS major at UT-Dallas. I'm an incoming intern at USAA, and teach kids how to code on the side. I'm aspiring to become a data science consultant, let's work on a project together!" />
+        <AboutCard name="Ariel Taylor" img="/src/images/ariel2.jpg" alt='Having a good time on a summer day' link='' desc="Hey there! I attend an institution called STEM where I was able to represent my country at two international robotics competitions. This is my first hackathon." />
         
       </div>
     </main>
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
   return (
     <div className="country-card rounded-xl inline-flex flex-col justify-start items-center p-4 m-2">
       <span className="font-bold text-2xl mb-2">{name}<br />{percent}</span>
-      <img src={img} className="mb-4 w-full" />
+      <img src={img} alt={'planting trees in ' + name} className="mb-4 w-full" />
       <ul>
         {
         inits.map((init) => (<li key={init.name} className='text-xl my-2'><a href={init.link} className='underline'>{init.name}</a></li>))
